f4781fc51aac45edbfd3db6c78a7093a8b0e98a4
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-05-07 12:59:52 -0400

    Improved cards with animations.

--- loaders/cards.js
+++ loaders/cards.js
@@ -1,3 +1,5 @@
-function cardURL(newURL) {
-    window.location.href = newURL
+function cardURL(id, newURL) {
+    $(`#card-${id}`).removeClass(['animated', 'zoomIn', 'faster', 'delay-0p25s', 'delay-0p5s', 'delay-0p75s', 'delay-1s', 'delay-1p25s', 'delay-1p5s', 'delay-1p75s', 'delay-2s'])
+    $(`#card-${id}`).addClass(['animated', 'pulse', 'faster'])
+    setTimeout(function() { window.location.href = newURL }, 250)
 }
