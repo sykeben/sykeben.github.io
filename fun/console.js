472a40ef96b18b9e5887a97ef017180eb9364b0e
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-05-07 19:17:28 -0400

    Added some fun.

---
+++ fun/console.js
@@ -0,0 +1,3 @@
+console.log('%c Wow! ', 'font-size: 100px; color: white; background-color: black; border-radius: 5px;')
+console.log('Looks like you must like code!')
+console.log('You should look at the source for this website: https://github.com/sykeben/sykeben.github.io')
