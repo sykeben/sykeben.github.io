4948d33c549e418d586731e1139874c6ddc579ad
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2021-03-18 14:28:12 -0400

    Added Connect4.

---
+++ fun/webgames/common/common.js
@@ -0,0 +1,10 @@
+// Setting Definitions
+const backLink = "../../../webgames.html";
+
+// Document Initialization
+$(document).ready(() => {
+
+    // Create back button (if applicable)
+    if ($("#back-container").length) $("#back-container").append(`<a id="back-button" href="${backLink}" tabindex="-1">&ltrif; Back</a>`);
+
+});
